80d90324dcf5c957c7c5782d4bd87d7ef3d00298
Benjamin Chodos <bdchodos@gmail.com>
2020-10-02 23:54:47 -0400

    Additional interfaces

---
+++ lib/Core/Interfaces/IAggregator.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Interfaces
+{
+	public interface IAggregator<TIn, TOut> : IReducer<TIn, TIn, TOut>
+	{
+	}
+}
---
+++ lib/Core/Interfaces/IReducer.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Interfaces
+{
+	public interface IReducer<TIn1, TIn2, TOut> : ITransformer<(TIn1, TIn2), TOut>
+	{
+		TOut Reduce(TIn1 arg1, TIn2 arg2);
+
+		TOut ITransformer<(TIn1, TIn2), TOut>.Transform((TIn1, TIn2) arg)
+			=> Reduce(arg.Item1, arg.Item2);
+	}
+}
