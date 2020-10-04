f59e08b925f4a71aa880d9b77f06f5fd5e015aa4
Benjamin Chodos <bdchodos@gmail.com>
2020-10-03 23:35:17 -0400

    Added specification interface

---
+++ lib/Core/Interfaces/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Interfaces
+{
+	public interface ISpecification<T> : ITransformer<T, bool>
+	{
+		bool IsSatisfiedBy(T arg);
+
+		bool ITransformer<T, bool>.Transform(T arg)
+			=> IsSatisfiedBy(arg);
+	}
+}
